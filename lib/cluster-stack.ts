505822dbb28fd7f245d66d3fda29fe659ab4fa48
Khanh Tran <trancongkhanh@gmail.com>
2022-05-23 17:40:19 -0400

    Update cluster-stack.ts

--- lib/cluster-stack.ts
+++ lib/cluster-stack.ts
@@ -1,6 +1,13 @@
-import { Fn, CfnResource, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as eks from 'aws-cdk-lib/aws-eks'
+import { CfnOutput, CfnResource, Arn, ArnFormat } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { Cluster, KubernetesVersion, Nodegroup } from 'aws-cdk-lib/aws-eks'
+import { InstanceType, IVpc } from 'aws-cdk-lib/aws-ec2';
+import {
+    IRole,
+    Role,
+    ServicePrincipal,
+    ManagedPolicy
+} from 'aws-cdk-lib/aws-iam'
 
 export class ClusterStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,7 +15,7 @@ export class ClusterStack extends Stack {
     
     const clusterRoleArn = Fn.importValue('clusterRoleArn')
     
-    new CfnResource(this, 'KubeStateMetrics', {
+    const resource = new CfnResource(this, 'KubeStateMetrics', {
       type: 'AWSQS::Kubernetes::Cluster',
       properties: {
         Name: 'eks-cluster-1',
@@ -28,5 +35,35 @@ export class ClusterStack extends Stack {
         }
       }
     })
+    
+    const clusterName = Arn.split(resource.getAtt('Arn').toString(), ArnFormat.SLASH_RESOURCE_NAME).resourceName
+    const cluster = Cluster.fromClusterAttributes(scope, 'imported-eks-cluster', {
+            clusterName: clusterName!,
+            clusterSecurityGroupId: 'sg-05b9eb28277988c95'
+            vpc: props.vpc
+        })
+
+        new Nodegroup(scope, 'medchem-eks-nodegroup', {
+            cluster: cluster,
+            nodeRole: this.nodeRole(scope),
+            minSize: 1,
+            desiredSize: 1,
+            maxSize: 1,
+            instanceTypes: [new InstanceType('t2.micro')]
+        })
   }
+  
+  
+  private nodeRole(scope: Construct): IRole {
+        return new Role(scope, 'medchem-eks-node-role', {
+            assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+            description: 'Role for EKS worker nodes',
+            managedPolicies: [
+                ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSWorkerNodePolicy'),
+                ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2ContainerRegistryReadOnly'),
+                ManagedPolicy.fromAwsManagedPolicyName('AmazonEKS_CNI_Policy'),
+                ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+            ]
+        })
+    }
 }
