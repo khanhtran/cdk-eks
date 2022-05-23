f070005a55f3e5ed478b5f97621ada32f4e09639
Khanh Tran <trancongkhanh@gmail.com>
2022-05-23 17:35:56 -0400

    Update type-activation-stack.ts

--- lib/type-activation-stack.ts
+++ lib/type-activation-stack.ts
@@ -1,76 +1,68 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { CfnTypeActivation } from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam'
-import { IAM_MINIMIZE_POLICIES } from 'aws-cdk-lib/cx-api';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Stack, StackProps, CfnTypeActivation } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import {
+    Role,
+    PolicyDocument,
+    Effect,
+    PolicyStatement,
+    CompositePrincipal,
+    ServicePrincipal
+} from 'aws-cdk-lib/aws-iam';
 
 export class TypeActivationStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    const executionRole = new iam.Role(this, 'eks-type-activation-role', {
-      roleName: 'eks-type-activation-role',
-      assumedBy: new iam.CompositePrincipal(
-        new iam.ServicePrincipal('resources.cloudformation.amazonaws.com'),
-        new iam.ServicePrincipal('lambda.amazonaws.com')
-      ),
-      inlinePolicies: {
-        'awsqs-resource-policy': this.activationRolePolicy()
-      }
-    })
-
-    new CfnTypeActivation(this, 'awsqs-eks-type-activation', {
-      autoUpdate: false,
-      typeName: 'AWSQS::EKS::Cluster',
-      majorVersion: '1',
-      executionRoleArn: executionRole.roleArn
-    })    
-  }
-
-  private activationRolePolicy(): iam.PolicyDocument {
-    return new iam.PolicyDocument({
-      statements: [
-        new PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          resources: ['*'],
-          actions: [
-            "secretsmanager:GetSecretValue",
-            "kms:Decrypt",
-            "eks:DescribeCluster",
-            "s3:GetObject",
-            "sts:AssumeRole",
-            "sts:GetCallerIdentity",
-            "iam:PassRole",
-            "ec2:CreateNetworkInterface",
-            "ec2:DescribeNetworkInterfaces",
-            "ec2:DeleteNetworkInterface",
-            "ec2:DescribeVpcs",
-            "ec2:DescribeSubnets",
-            "ec2:DescribeRouteTables",
-            "ec2:DescribeSecurityGroups",
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents",
-            "lambda:UpdateFunctionConfiguration",
-            "lambda:DeleteFunction",
-            "lambda:GetFunction",
-            "lambda:InvokeFunction",
-            "lambda:CreateFunction",
-            "lambda:UpdateFunctionCode",
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:DescribeLogGroups",
-            "logs:DescribeLogStreams",
-            "logs:PutLogEvents",
-            "cloudwatch:ListMetrics",
-            "cloudwatch:PutMetricData",
-            "ssm:PutParameter",
-            "ssm:GetParameter",
-            "ssm:DeleteParameter"
-          ]
+    constructor(scope: Construct, id: string, props: StackProps) {
+        super(scope, id, props)
+        const executionRole = new Role(this, 'eks-type-activation-role', {
+            roleName: 'eks-type-activation-role',
+            assumedBy: new CompositePrincipal(
+                new ServicePrincipal('resources.cloudformation.amazonaws.com'),
+                new ServicePrincipal('lambda.amazonaws.com')
+            ),
+            inlinePolicies: {
+                'awsqs-resource-policy': this.activationRolePolicy()
+            }
         })
-      ]
-    })
-  }
+
+        new CfnTypeActivation(this, 'awsqs-eks-type-activation', {
+            executionRoleArn: executionRole.roleArn,
+            publicTypeArn: 'arn:aws:cloudformation:us-east-2::type/resource/408988dff9e863704bcc72e7e13f8d645cee8311/AWSQS-EKS-Cluster'
+        })
+    }
+
+
+    private activationRolePolicy(): PolicyDocument {
+        return new PolicyDocument({
+            statements: [
+                new PolicyStatement({
+                    effect: Effect.ALLOW,
+                    resources: ['*'],
+                    actions: [
+                        'cloudformation:ListExport',
+                        'ec2:DescribeSecurityGroups',
+                        'ec2:DescribeSubnets',
+                        'ec2:DescribeVpcs',
+                        'eks:CreateCluster',
+                        'eks:DeleteCluster',
+                        'eks:DescribeCluster',
+                        'eks:ListTagsForResource',
+                        'eks:TagResource',
+                        'eks:UntagResource',
+                        'eks:UpdateClusterConfig',
+                        'eks:UpdateClusterVersion',
+                        'iam:PassRole',
+                        'kms:CreateGrant',
+                        'kms:DescribeKey',
+                        'lambda:CreateFunction',
+                        'lambda:DeleteFunction',
+                        'lambda:GetFunction',
+                        'lambda:InvokeFunction',
+                        'lambda:UpdateFunctionCode',
+                        'lambda:UpdateFunctionConfiguration',
+                        'sts:AssumeRole',
+                        'sts:GetCallerIdentity'
+                    ]
+                })
+            ]
+        })
+    }
 }
